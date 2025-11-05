24bea847883b7508bac48eda783947cafbe63a12
Melarmony <ana.viktorenko@yandex.ru>
2025-11-05 17:56:33 +0300

    FSM Walk

---
+++ Bartender_MVP/Assets/Scripts/FSM/FSMStateWalk.cs
@@ -0,0 +1,25 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Assets.Scripts.FSM
+{
+    public class FSMStateWalk : FSMState
+    {
+        public FSMStateWalk(global::FSM fsm) : base(fsm) { }
+
+        public override void Enter()
+        {
+            Debug.Log("Walk state ENTER");
+        }
+
+        public override void Exit() 
+        {
+            Debug.Log("Walk state EXIT");
+        }
+
+        public override void Update() 
+        {
+            Debug.Log("Walk state UPDATE");
+        }
+    }
+}
